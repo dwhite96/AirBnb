6189009731c936d9dee3ba1447585a6902e32f8d
David White <whited75@gmail.com>
2014-11-12 22:58:49 -0700

    Modify rspec tests for routing files

--- spec/routing/booking_requests_routing_spec.rb
+++ spec/routing/booking_requests_routing_spec.rb
@@ -4,31 +4,31 @@ RSpec.describe BookingRequestsController, :type => :routing do
   describe "routing" do
 
     it "routes to #index" do
-      expect(:get => "/booking_requests").to route_to("booking_requests#index")
+      expect(:get => "/listings/1/booking_requests").to route_to("booking_requests#index", :listing_id => "1")
     end
 
     it "routes to #new" do
-      expect(:get => "/booking_requests/new").to route_to("booking_requests#new")
+      expect(:get => "/listings/1/booking_requests/new").to route_to("booking_requests#new", :listing_id => "1")
     end
 
     it "routes to #show" do
-      expect(:get => "/booking_requests/1").to route_to("booking_requests#show", :id => "1")
+      expect(:get => "/listings/1/booking_requests/1").to route_to("booking_requests#show", :listing_id => "1", :id => "1")
     end
 
     it "routes to #edit" do
-      expect(:get => "/booking_requests/1/edit").to route_to("booking_requests#edit", :id => "1")
+      expect(:get => "/listings/1/booking_requests/1/edit").to route_to("booking_requests#edit", :listing_id => "1", :id => "1")
     end
 
     it "routes to #create" do
-      expect(:post => "/booking_requests").to route_to("booking_requests#create")
+      expect(:post => "/listings/1/booking_requests").to route_to("booking_requests#create", :listing_id => "1")
     end
 
     it "routes to #update" do
-      expect(:put => "/booking_requests/1").to route_to("booking_requests#update", :id => "1")
+      expect(:put => "/listings/1/booking_requests/1").to route_to("booking_requests#update", :listing_id => "1", :id => "1")
     end
 
     it "routes to #destroy" do
-      expect(:delete => "/booking_requests/1").to route_to("booking_requests#destroy", :id => "1")
+      expect(:delete => "/listings/1/booking_requests/1").to route_to("booking_requests#destroy", :listing_id => "1", :id => "1")
     end
 
   end
